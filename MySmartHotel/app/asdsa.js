3cd815937532a3ea85264e69ee1fa6e491c68595
Alfonso Reyes Cortés <arc980103@gmail.com>
2017-06-24 12:05:42 -0600

    Eliminación archivos basura

--- MySmartHotel/app/asdsa.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXNkc2EuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJhc2RzYS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlc0NvbnRlbnQiOlsiIl19
