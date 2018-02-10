624acd5dea728fe5610a854eb75c25d899e4d547
Alfonso Reyes Cortés <arc980103@gmail.com>
2018-02-10 01:37:05 -0700

    Removidos .js basura

--- MySmartHotel/app/models/location.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Location = (function () {
-    function Location() {
-    }
-    return Location;
-}());
-exports.Location = Location;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibG9jYXRpb24ubW9kZWwuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJsb2NhdGlvbi5tb2RlbC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBO0lBQUE7SUFHQSxDQUFDO0lBQUQsZUFBQztBQUFELENBQUMsQUFIRCxJQUdDO0FBSFksNEJBQVEiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY2xhc3MgTG9jYXRpb24ge1xyXG4gIGxhdGl0dWRlIDogbnVtYmVyO1xyXG4gIGxvbmdpdHVkZSA6IG51bWJlcjtcclxufVxyXG4iXX0=
